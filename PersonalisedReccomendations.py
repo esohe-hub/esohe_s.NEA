29f4a09649dfc8c9f656c77784c48764e831c609
esohe <esoheedevbie@gmail.com>
2026-02-19 11:23:18 +0000

    esohe_s.NEA/SQLite database creation.py

--- PersonalisedReccomendations.py
+++ PersonalisedReccomendations.py
@@ -3,12 +3,15 @@ conn = sqlite3.connect('MovieRecommendationSystem.db')
 cursor = conn.cursor()
 
 user_input = input("Please enter: Y if you would like a personalised recommendation, N if you would like a random recommendation and E to exit the program.").upper()
-if user_input == "Y":
-    #blah blah
-    cursor.execute
+'''if user_input == "Y":
+   
 elif user_input == "N":
     #blah blah
 elif user_input == "E":
     print("Thank you for using my recommendation system. Goodbye!")
 else:
-print("Invalid input. Please enter Y, N, or E.")
+print("Invalid input. Please enter Y, N, or E.")'''
+
+#selecting a movie based off of rating
+if user_input == "Y":
+    SELECT RatingId, Rating FROM Ratings WHERE UserId = ? ORDER BY Rating DESC
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NEA Movie Reccomendation System Project
 Creating an SQL database containing a Movies table, Users table, Ratings table.
-Commit the changes & close the connection
+Provide personalised recommendations to the user based on inputted  data such as genre and rating. Recommend random movies to user and provide inforation on pre-stored movies such as concise summaries, ratings, cast etc.
